ae3f511c78a9c08148a3ffd075269415c6188631
Piwoksn <piwoksn@gmail.com>
2023-03-18 03:38:01 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main -Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n += 1;
+	}
+	return (0);
+}
